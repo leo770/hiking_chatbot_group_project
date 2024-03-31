79273b7f216c858376f7920af5406371af32177c
leo770 <75868156+leo770@users.noreply.github.com>
2024-04-01 00:52:51 +0800

    Update Test_ChatbotV3.py

--- Test_ChatbotV3.py
+++ Test_ChatbotV3.py
@@ -39,8 +39,8 @@ def get_config():
 def submit(message):
     config = get_config()
     conversation = [{"role": "user", "content": message}]
-    url = (self.config['CHATGPT']['BASICURL']) + "/deployments/" + (self.config['CHATGPT']['MODELNAME']) + "/chat/completions/?api-version=" + (self.config['CHATGPT']['APIVERSION'])
-    headers = { 'Content-Type': 'application/json', 'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
+    url = config['BASICURL'] + "/deployments/" + config['MODELNAME'] + "/chat/completions/?api-version=" + config['APIVERSION']
+    headers = { 'Content-Type': 'application/json', 'api-key': config['ACCESS_TOKEN'] }
     payload = {'messages': conversation}
     response = requests.post(url, json=payload, headers=headers)
     if response.status_code == 200:
